6305b4ee7e8629e0d2683347e0ba25af71367aac
Carol Wargo <Carolwargo@gmail.com>
2023-07-28 10:41:12 -0400

    added Home Route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Welcome to 301_catching</h1>
+            <p>Master your craft- Elevate your game.</p>
         </div>
     )
 }
