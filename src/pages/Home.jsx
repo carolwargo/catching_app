b81ef03e4f80d13648bc21cdd6dbe3967241737c
Carol Wargo <Carolwargo@gmail.com>
2023-07-28 19:35:09 -0400

    spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 import "../Styles/Home.css"
+
+
+
+
 
 export default function Home() {
   return (
