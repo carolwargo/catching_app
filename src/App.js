a1ec243d1ed8e0732f4cd9cae9d35f9c8ede7188
Carol Wargo <Carolwargo@gmail.com>
2023-07-28 19:13:11 -0400

    installs and navlink

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
 import './App.css';
 
 export default function App() {
   return(
     <Router>
+      <Navbar />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route  path="/about" element={<About />} />   
       </Routes>
+      <Footer />
     </Router>
   ) 
 }
