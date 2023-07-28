a85f57de3cf51a77df25ab0fcbe93195fa8285ac
Carol Wargo <Carolwargo@gmail.com>
2023-07-28 12:39:17 -0400

    removed nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import Navbar from "../components/Navbar";
+import React from 'react';
+
 export default function Home() {
     return (
-        <div>
-            <Navbar/>
-            <h1>Welcome to 301_catching</h1>
-            <p>Master your craft- Elevate your game.</p>
+        <div className="container-body">
+            <div className="left-col">
+                <div className="content">
+                    <h1>Welcome to 301_catching</h1>
+                    <p>Master your craft- Elevate your game.</p>
+                </div>
+            </div>  
+
+           <div className="right-col">
+                <div className="content-form">
+                    <div class="input-wrapper">
+                    <label for="name"></label>
+                    <input type="text"></input>
+                </div>
+
+                <div class="input-wrapper">
+                    <label for="email"></label>
+                    <input type="text"></input>
+                </div>
+
+                <div class="input-wrapper">
+                    <button>Sign me up</button>
+                </div>
+
+           </div>
+            
         </div>
+    </div>
+       
     )
 }
