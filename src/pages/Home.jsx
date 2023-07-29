fcb1e5f65443a31b1b839ca55c8d30513f3ccb93
Carol Wargo <Carolwargo@gmail.com>
2023-07-29 16:25:42 -0400

    added styling and installs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,102 @@ import React from "react";
 import "../Styles/Home.css";
 import { Link } from "react-router-dom";
 import Contact from "./Contact";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function Home() {
   return (
     <>
-
-    
       <div className="container-body">
         <div className="left-col">
-          <div className="content">
-            <h1>301_catching</h1>
-           <h3>FOUNDATIONAL EXPERTS</h3>
-            <p>Master your craft & elevate your game</p>
+          <div className="content-container">
+            <div className="content"> 
+            <h2>WELCOME TO</h2>
+              <h1>301_catching</h1>
+              <h3>THE FOUNDATIONAL EXPERTS for everything catching-</h3>
+              <p>Helping to "Master your craft & elevate your game."</p>
+            </div>
             <div className="input-btn">
-              <Link to= {Contact}><button>Sign me up</button></Link> 
+              <Link to={Contact}>
+                <button>Sign me up</button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
+
+      <Row className="info-row">
+        <Col className="register-form-col">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+
+        <Col className="column-right">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col className="column-left">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 }
--- src/pages/Train.jsx
+++ src/pages/Train.jsx
@@ -1,16 +1,28 @@
+import React from 'react';
+
 import Form from 'react-bootstrap/Form';
 
 function FormFileExample() {
   return (
-    <div className='evaluation-container'>
-    <h1>Online Evaluations</h1>
-    <h3>Upload your video to be evaluated by Coach Corey</h3>
+    <div className='train-container'>
+    <h1>TRAIN LIKE A CAVEMAN</h1>
+    <br></br>
     <div className='evaluation-information'>  
-        <h4>In-Person</h4>
-        <h4>Video Upload</h4>
-            <p># Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum at repellat, nulla, ipsa eius beatae cupiditate neque impedit numquam sed eaque. Quis qui vero ullam amet nobis voluptates blanditiis.</p>
-            <p>## Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem facilis vel aut, odio odit nisi, deleniti doloremque, a consectetur eos eum nobis enim dolorem sed maiores in temporibus iste tempora.</p>
+        <h2>IN-PERSON TRAINING</h2>
             
+        <h5>How it works</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ea quae quos, eaque aliquam perspiciatis impedit molestiae iste repudiandae dolore voluptatibus minus non facere voluptatum necessitatibus ducimus veritatis distinctio saepe!</p>
+        <h5>What you get</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi delectus explicabo nam? Quas eius consectetur explicabo ex fuga illum tempora voluptate nobis labore sit rem voluptatum, laudantium culpa voluptatem dignissimos?</p>
+        <h5>Pricing</h5>
+            <p>1 video - $25</p>
+        <h5>What you get</h5>
+            <p>1. 10 minute video evaluation</p>
+
+            <br></br>
+        <h2>VIRTUAL EVALUATION</h2>
+        <h4>Video Upload</h4>
+
         <h5>How it works</h5>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ea quae quos, eaque aliquam perspiciatis impedit molestiae iste repudiandae dolore voluptatibus minus non facere voluptatum necessitatibus ducimus veritatis distinctio saepe!</p>
         <h5>What you get</h5>
@@ -24,22 +36,19 @@ function FormFileExample() {
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Upload your video</Form.Label>
         <Form.Control type="file" />
+        <button type="submit">Submit</button>
       </Form.Group>
+
       <Form.Group controlId="formFileMultiple" className="mb-3">
         <Form.Label>Upload video</Form.Label>
         <Form.Control type="file" multiple />
+        <button type="submit">Submit</button>
       </Form.Group>
-      <Form.Group controlId="formFileDisabled" className="mb-3">
-        <Form.Label>Disabled file input example</Form.Label>
-        <Form.Control type="file" disabled />
-      </Form.Group>
-      <Form.Group controlId="formFileSm" className="mb-3">
-        <Form.Label>Upload small video option</Form.Label>
-        <Form.Control type="file" size="sm" />
-      </Form.Group>
+      
       <Form.Group controlId="formFileLg" className="mb-3">
         <Form.Label>Upload large video option</Form.Label>
         <Form.Control type="file" size="lg" />
+        <button type="submit">Submit</button>
       </Form.Group>
     </div>
     </div> 
