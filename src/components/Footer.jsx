060769bf739fa90a99071327a6edc73dddb44e93
Carol Wargo <Carolwargo@gmail.com>
2023-07-31 08:47:14 -0400

    added social media to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import "../Styles/Footer.css"
 export default function Footer() {
     return(
 
-        <footer className="footer-copyright">&#169; 2023 #301_catching</footer>
+        <Footer className="footer-copyright">&#169; 2023 #301_catching</Footer>
 
     )
 }
