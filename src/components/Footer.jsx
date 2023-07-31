814c2a96e19b197ef2b59d83affef0cb2faaf106
Carol Wargo <Carolwargo@gmail.com>
2023-07-31 09:29:41 -0400

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ import "../Styles/Footer.css"
 export default function Footer() {
     return(
 
-        <Footer className="footer-copyright">&#169; 2023 #301_catching</Footer>
+        <footer className="footer-copyright">
+            &#169; 2023  301_catching
+            
+        </footer>
 
     )
 }
