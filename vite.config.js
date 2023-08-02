aff72796e67e88f8f86d0befd4fd3258c87a3fdd
Carol Wargo <Carolwargo@gmail.com>
2023-08-02 16:29:38 -0400

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import envCompatible from 'vite-plugin-env-compatible';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { NodeGlobalsPolyfillPlugin } from 'esbuild-plugins/node-globals-polyfill';
-import svgrPlugin from 'vite-plugin-svgr'; // Correct import name
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  envPrefix: 'REACT_APP_',
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
-  plugins: [
-    react(),
-    envCompatible(),
-    tsconfigPaths(),
-    svgrPlugin({ // Correct plugin name
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
-});
+	plugins: [react()]	
+})
