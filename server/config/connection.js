ea324215042d5b485ab3478c3c85d994cb3c7d16
Carol Wargo <Carolwargo@gmail.com>
2023-08-02 12:19:16 -0400

    fixed mongo config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(import.meta.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/301_catching');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/301_catching', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
+const db = mongoose.connection;
 
-
-
-module.exports = mongoose.connection;
+module.exports = db;
