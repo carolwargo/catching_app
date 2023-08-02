1f3769398382410a818896b9ab16343fe0b33393
Carol Wargo <Carolwargo@gmail.com>
2023-08-02 12:17:37 -0400

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const config = require('./config');
 const mongoose = require('mongoose');
-const {typeDefs, resolvers} = require('./schema');
-const { viteMiddleware, createViteDevServer } = require('./viteMiddleware'); 
+const { typeDefs, resolvers } = require('./schema');
 
-const app= express();
-const PORT = import.meta.env.REACT_APP_NAME || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -21,29 +20,23 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (import.meta.env.NODE_ENV === 'production') {  
-  createViteDevServer().then((viteDevServer) => {
-    app.use(viteMiddleware);
-    server.applyMiddleware({ app });
-  });
-} else {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-  server.applyMiddleware({ app });
 }
 
 //Create new instance of Apollo server and pass in schema data
 const startApolloServer = async () => {
   await server.start();
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+    });
+  });
 };
 
 //Call the async function to start server
@@ -59,9 +52,3 @@ mongoose.connect(config.MONGODB_URI, {
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,37 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import envCompatible from 'vite-plugin-env-compatible';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {NodeGlobalsPolyfillPlugin} from 'esbuild-plugins/node-globals-polyfill';
-import svgr from 'vite-plugin-svgr';
+import { NodeGlobalsPolyfillPlugin } from 'esbuild-plugins/node-globals-polyfill';
+import svgr from 'vite-plugin-svgr'; // Correct import name
 
 export default defineConfig({
-    envPrefix: 'REACT_APP_',
-    resolve: {
-        alias: {
-            './runtimeConfig': './runtimeConfig.browser',
-        },
+  envPrefix: 'REACT_APP_',
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
     },
-
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: 'globalThis',
-            },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                }),
-            ],
-            },
-    },
-    plugins: [
-        react(),
-        envCompatible(),
-        tsconfigPaths(),
-        svvgrPlugin({
-            svgrOptions: {
-                icon: true,
-            },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
         }),
-    ]    
+      ],
+    },
+  },
+  plugins: [
+    react(),
+    envCompatible(),
+    tsconfigPaths(),
+    svgr({ // Correct plugin name
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
